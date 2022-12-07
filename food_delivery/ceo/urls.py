2e6596ca4aac88ff49b365a7a3cf0ecdd9fb8508
whddla <whddla8912@gmail.com>
2022-12-07 17:47:48 +0900

    매장관리 추가
    매장관리 추가

--- food_delivery/ceo/urls.py
+++ food_delivery/ceo/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('order/', views.order),
+    path('progress/', views.progress),
+    path('completion/', views.completion),
+    path('detail/', views.detail),
     path('manage/', views.manage),
 ]
--- food_delivery/ceo/views.py
+++ food_delivery/ceo/views.py
@@ -6,3 +6,12 @@ def order(request):
 
 def manage(request):
     return render(request, "myStore/manage.html")
+
+def progress(request):
+    return render(request, "myStore/progress.html")
+
+def completion(request):
+    return render(request, "myStore/completion.html")
+    
+def detail(request):
+    return render(request, "myStore/detail.html")
